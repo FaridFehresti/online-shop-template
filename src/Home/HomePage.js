c8552abedb8d7c55a88250c7c5d4602601e1f748
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2024-02-08 20:40:37 +0330

    created project

---
+++ src/Home/HomePage.js
@@ -0,0 +1,17 @@
+// src/Home/HomePage.js
+
+import React from "react";
+import Header from "./components/Header";
+import "./HomePage.css";
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="haeder-container">
+                <Header />
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/Home/components/Header.js
@@ -0,0 +1,15 @@
+// src/Home/components/Header.js
+
+import React from "react";
+import "./Header.css"; // Assume a corresponding CSS file for styling
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>My Online Shop</h1>
+            // Other header content
+        </header>
+    );
+};
+
+export default Header;
